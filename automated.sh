2c5764df0de0d260bcc7c49ff3863b16cf95b591
Nathan <njpthepirate@yahoo.com>
2016-08-18 01:48:11 -0400

    Update automated.sh

--- automated.sh
+++ automated.sh
@@ -3,7 +3,7 @@ echo "Coded by Nathan Parker"
 python pytubetest.py
 ffmpeg -i video/video.mp4 songs/song.mp3
 
-rm /Users/nateparker/Desktop/Proximity/video/video.mp4
+rm video/video.mp4
 echo "#################################"
 echo "######### ALL FINISHED ##########"
 echo "#################################"
